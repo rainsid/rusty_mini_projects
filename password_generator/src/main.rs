8fa41cb8447adf0ca3d68eb0569a318b95be7b51
rainsid <adrianroycidro@gmail.com>
2025-03-08 10:17:14 +0800

    parsing and validating input

--- password_generator/src/main.rs
+++ password_generator/src/main.rs
@@ -3,9 +3,51 @@ use std::env;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    // args[0] is the program path
-    let pass_length = &args[1];
-    let pass_complexity = &args[2];
+    if args.len() != 3 {
+        // print error message and exit
+        eprintln!("Error: Please provide two argumenst: <length> <complexity> ");
+        eprintln!("Usage: ./password_generator <length> <complexity>");
+        std::process::exit(1);
+    }
 
-    println!("{}, {}", pass_length, pass_complexity);
+    //validate input
+    // args[0] is the program path
+    let pass_length: u8 = match args[1].parse() {
+        Ok(n) => n,
+        Err(_) => {
+            eprintln!("Error: Password length must be a number between 8 and 32");
+            std::process::exit(1);
+        }
+    };
+
+    let pass_complexity: u8 = match args[2].parse() {
+        Ok(n) => n,
+        Err(_) => {
+            eprintln!("Error: Password complexity must be a number between 1 and 3");
+            std::process::exit(1);
+        }
+    };
+
+    // if _pass_length > 32 || _pass_length < 8 {
+    //     eprintln!("Error: Password length must be between 8 and 32");
+    //     std::process::exit(1);
+    // }
+    //
+    // if _pass_complexity > 3 || _pass_complexity < 1 {
+    //     eprintln!("Error: Password complexity must be between 1 and 3");
+    //     std::process::exit(1);
+    // }
+    // println!("{}, {}", pass_length, pass_complexity);
+    //
+    //   I want to be idiomatic here. Rust analyzer (Clippy) keeps complaining
+
+    if !(8..=32).contains(&pass_length) {
+        eprintln!("Error: Password length must be between 8 and 32");
+        std::process::exit(1);
+    }
+
+    if !(1..=3).contains(&pass_complexity) {
+        eprintln!("Error: Password complexity must be between 2 and 3");
+        std::process::exit(1);
+    }
 }
