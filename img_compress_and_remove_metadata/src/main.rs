cd3c3e591048f6bc2635f8c863f132c34a439923
rain <adrianroycidro@gmail.com>
2025-07-30 09:40:03 +0800

    path to string

--- img_compress_and_remove_metadata/src/main.rs
+++ img_compress_and_remove_metadata/src/main.rs
@@ -1,5 +1,10 @@
+use core::panic::PanicInfo;
 use std::env;
+use std::ffi::os_str;
 use std::fs;
+use std::path::Path;
+use std::path::PathBuf;
+use std::process::Command;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -11,11 +16,25 @@ fn main() {
         Ok(entries) => {
             for entry in entries {
                 match entry {
-                    Ok(entry) => println!("{:?}", entry.path()),
+                    Ok(entry) => {
+                        if let Some(s) = path_to_string(&entry.path()) {
+                            println!("{s}");
+                            remove_metadata(&s);
+                        }
+                    }
+
                     Err(e) => eprintln!("Error: {e}"),
                 }
             }
         }
         Err(e) => eprintln!("Error: {e}"),
     }
+}
+
+fn path_to_string(path: &Path) -> Option<String> {
+    path.to_path_buf().into_os_string().into_string().ok()
+}
+
+fn remove_metadata(path: &String) {
+    println!("{path}");
 }
