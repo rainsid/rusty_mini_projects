3d57569d3bd0e82973c1a3f2a8ab9c5c7f108423
rain <adrianroycidro@gmail.com>
2025-08-03 11:37:53 +0800

    read coordinates file

--- geotag_img/src/main.rs
+++ geotag_img/src/main.rs
@@ -1,11 +1,15 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
 use std::path::Path;
 use std::{env, fs};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let dir = &args[1];
+    let coord_file = &args[2];
+    let mut coordinates: Vec<String>;
 
-    println!("{dir}");
+    coordinates = read_coordinates_file(coord_file);
 
     match fs::read_dir(dir) {
         Ok(entries) => {
@@ -28,4 +32,17 @@ fn main() {
 
 fn path_to_string(path: &Path) -> Option<String> {
     path.to_path_buf().into_os_string().into_string().ok()
+}
+
+fn read_coordinates_file(file_path: &String) -> Vec<String> {
+    let file = File::open(file_path).expect("Could not open file");
+    let reader = BufReader::new(file);
+    let mut coordinates: Vec<String> = Vec::new();
+
+    for line in reader.lines() {
+        let line = line.expect("Could read line");
+        coordinates.push(line);
+    }
+
+    coordinates
 }
